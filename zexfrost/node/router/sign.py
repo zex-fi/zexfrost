cc25af932874145c89314f826437ca78c9e5e6f7
pooya <pooyafekri79@gmail.com>
2025-05-28 16:15:41 +0330

    fix(repository):Add node_id to repo keys

--- zexfrost/node/router/sign.py
+++ zexfrost/node/router/sign.py
@@ -4,6 +4,7 @@ from zexfrost.custom_types import Commitment, CommitmentRequest
 from zexfrost.utils import get_curve
 
 from ..repository import get_key_repository, get_nonce_repository
+from ..settings import node_settings as settings
 from ..sign import commitment as signature_commitment
 
 router = APIRouter(prefix="/sign")
@@ -12,6 +13,7 @@ router = APIRouter(prefix="/sign")
 @router.post("/commitment", response_model=Commitment)
 async def commitment(commitment_request: CommitmentRequest):
     return signature_commitment(
+        node_id=settings.ID,
         curve=get_curve(commitment_request.curve),
         key_repo=get_key_repository(),
         nonce_repo=get_nonce_repository(),
